93fd8786ecee3460884e8c34627227ab96cad2b9
Marshal Marks <81783232+MarshalMarks@users.noreply.github.com>
2023-03-29 18:21:48 -0500

    Added comments
    Made code slightly easier to read

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Define element variables
 const computerChoiceText = document.getElementById("computerChoiceText");
 const playerScoreText    = document.getElementById("playerScoreText");
 const computerScoreText  = document.getElementById("computerScoreText");
@@ -7,40 +8,46 @@ const buttonRock         = document.getElementById("buttonRock");
 const buttonPaper        = document.getElementById("buttonPaper");
 const buttonScissors     = document.getElementById("buttonScissors");
 
-const rock     = 0;
-const paper    = 1;
-const scissors = 2;
-
-let playerScore    = 0;
-let computerScore  = 0;
+// Define move numbers and related variables
+const rock         = 0;
+const paper        = 1;
+const scissors     = 2;
 let playerChoice   = 0;
 let computerChoice = 0;
-let maxWins        = 5;
 
-// Return random interval including the min and max
+// Define score-related variables
+const maxMoves     = 5;
+let playerScore    = 0;
+let computerScore  = 0;
+
+// Inclusive of min and max
 function randomIntFromInterval(min, max) 
 {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// Take a move type as input and return the move name as a string
 function nameFromNumber(inputNumber)
 {
     return (inputNumber == 0 ? "Rock" : inputNumber == 1 ? "Paper" : "Scissors");
 }
 
+// Enable or disable input buttons
 function setButtonsDisabled(inputBoolean)
 {
-    buttonRock.disabled = inputBoolean;
-    buttonPaper.disabled = inputBoolean;
+    buttonRock.disabled     = inputBoolean;
+    buttonPaper.disabled    = inputBoolean;
     buttonScissors.disabled = inputBoolean;
 }
 
+// Computer makes their move
 function computerChoose()
 {
     computerChoice = randomIntFromInterval(0,2);
     computerChoiceText.innerText = nameFromNumber(computerChoice);
 }
 
+// Disable input buttons and display the victory message
 function endGame(winner)
 {
     setButtonsDisabled(true);
@@ -48,14 +55,7 @@ function endGame(winner)
     message.innerText = message.innerText + " " + winner + " wins!";
 }
 
-function checkScore(winner)
-{
-    if(playerScore >= Math.ceil(maxWins/2) || computerScore >= Math.ceil(maxWins/2))
-        endGame(winner);
-    else
-        message.innerText = message.innerText + " " + winner + " gains a point.";
-}
-
+// Handle win, lose, and tie conditions
 function winMatch()
 {
     playerScore++;
@@ -77,6 +77,15 @@ function tieMatch()
     message.innerText = "It's a tie!";
 }
 
+// End game if someone's score reaches 3
+function checkScore(winner)
+{
+    if(playerScore >= Math.ceil(maxMoves/2) || computerScore >= Math.ceil(maxMoves/2))
+        endGame(winner);
+    else
+        message.innerText = message.innerText + " " + winner + " gains a point.";
+}
+
 function evaluateMatch(inputPlayerChoice)
 {
     // Set global choice variables
@@ -92,6 +101,7 @@ function evaluateMatch(inputPlayerChoice)
         tieMatch();
 }
 
+// Set up conditions for a new game to start
 function newGame()
 {
     playerScore = 0;
